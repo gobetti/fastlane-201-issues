c38f43c6e17506ccd2dea4c3eae3d413e652bb65
Marcelo Gobetti <mwgobetti@gmail.com>
2022-01-21 17:20:12 -0300

    works: bundle exec fastlane test

---
+++ fastlane-201-issues/fastlane_201_issuesApp.swift
@@ -0,0 +1,17 @@
+//
+//  fastlane_201_issuesApp.swift
+//  fastlane-201-issues
+//
+//  Created by Marcelo Gobetti on 21/01/22.
+//
+
+import SwiftUI
+
+@main
+struct fastlane_201_issuesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fastlane-201-issuesTests/fastlane_201_issuesTests.swift
@@ -0,0 +1,17 @@
+//
+//  fastlane_201_issuesTests.swift
+//  fastlane-201-issuesTests
+//
+//  Created by Marcelo Gobetti on 21/01/22.
+//
+
+import XCTest
+@testable import fastlane_201_issues
+
+class fastlane_201_issuesTests: XCTestCase {
+
+    func testExample() throws {
+        XCTFail()
+    }
+
+}
